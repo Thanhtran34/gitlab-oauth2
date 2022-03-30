87ad6f4e417e0fb0ccec71bdd616f080b5d1e9d3
Thanh Tran <tt222kj@student.lnu.se>
2022-03-30 14:31:43 +0200

    fix image security

--- src/server.js
+++ src/server.js
@@ -20,8 +20,8 @@ const main = async () => {
         directives: {
           ...helmet.contentSecurityPolicy.getDefaultDirectives(),
           'default-src': ["'self'"],
-          'script-src': ['https://gitlab.lnu.se', 'cdn.jsdelivr.net', 'code.jquery.com', 'cdn.jsdelivr.net', 'cdnjs.cloudflare.com'],
-          'img-src': ['https://gitlab.lnu.se', '*.gravatar.com', 'cdn.jsdelivr.net']
+          'script-src': ["'self'",'https://gitlab.lnu.se', 'cdn.jsdelivr.net', 'code.jquery.com', 'cdn.jsdelivr.net', 'cdnjs.cloudflare.com'],
+          'img-src': ["'self'",'https://gitlab.lnu.se', '*.gravatar.com', 'cdn.jsdelivr.net']
         }
       },
       crossOriginResourcePolicy: { policy: 'cross-origin' },

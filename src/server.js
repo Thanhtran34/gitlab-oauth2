a39294e828dda24658127c4c6311320677101d25
Thanh Tran <tt222kj@student.lnu.se>
2022-03-30 14:29:48 +0200

    try fix helmet

--- src/server.js
+++ src/server.js
@@ -21,7 +21,7 @@ const main = async () => {
           ...helmet.contentSecurityPolicy.getDefaultDirectives(),
           'default-src': ["'self'"],
           'script-src': ['https://gitlab.lnu.se', 'cdn.jsdelivr.net', 'code.jquery.com', 'cdn.jsdelivr.net', 'cdnjs.cloudflare.com'],
-          'img-src': ["'self'", 'https://gitlab.lnu.se', '*.gravatar.com', 'cdn.jsdelivr.net']
+          'img-src': ['https://gitlab.lnu.se', '*.gravatar.com', 'cdn.jsdelivr.net']
         }
       },
       crossOriginResourcePolicy: { policy: 'cross-origin' },
